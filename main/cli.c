27c5fb138a24144ccd6caebd3d9c8f256e03be21
Florian-Hagemann <florianhagemann42@gmail.com>
2026-05-30 19:46:07 +0200

    finished

--- main/cli.c
+++ main/cli.c
@@ -7,11 +7,54 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+
 static void serial_cli_task(void *pvParameters) {
+    
+    int idx = 0;
+    char input_buffer[20];
+
+    memset(input_buffer, 0, sizeof(input_buffer));
+    int input;
+
+    while(1) {
+        input = getchar();
+        if (input == EOF || input < 0) {
+            vTaskDelay(10 / portTICK_PERIOD_MS); // Sleep for 10ms
+            continue;                            // Jump back to the top of while(1)
+        }
+        putchar(input);
+
+        if(input != '\n' && input != '\r') { // Added '\r' check for different terminals
+            if (idx < sizeof(input_buffer) - 1) {
+                input_buffer[idx] = input;
+                idx++;
+            }
+        } else {
+
+            if(strcmp(input_buffer, "temp") == 0) {
+                printf("The internal temperature is %.2f \n", get_latest_temperature());
+            } else {
+                printf("Unknown command: use \"temp\" to read internal temp.\n");
+            }
+
+            memset(input_buffer, 0, sizeof(input_buffer));
+            idx = 0;
+
+        }
+
+    }
     // TODO: get user input
+
     // TODO: if input == "temp" call get_latest_temperature() and output that
 }
 
 void start_cli(void) {
-    // TODO: create task for serial_cli_task
+    xTaskCreate(
+        serial_cli_task,
+        "Serial CLI Task",
+        2064,
+        NULL,
+        1,
+        NULL
+    );
 }
--- main/temp_sensor.c
+++ main/temp_sensor.c
@@ -5,10 +5,35 @@
 
 static volatile float latest_temp = 0.0f;
 
-float get_latest_temperature(void *pvParameters) {
+static void temp_monitor_task(void *pvParameters) {
+    temperature_sensor_handle_t temp_sensor = NULL;
+    temperature_sensor_config_t temp_sensor_config = TEMPERATURE_SENSOR_CONFIG_DEFAULT(-10, 80);
+
+    temperature_sensor_install(&temp_sensor_config, &temp_sensor);
+
+    temperature_sensor_enable(temp_sensor);
+
+    while(1) {
+        float tsens_out;
+        
+        if (temperature_sensor_get_celsius(temp_sensor, &tsens_out) == ESP_OK) {
+            latest_temp = tsens_out; 
+        }
+
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+}
+float get_latest_temperature(void) {
     return latest_temp;
 }
 
 void start_temperature_monitoring(void) {
-    // TODO: start task for temperature monitoring 
+    xTaskCreate(
+        temp_monitor_task,
+        "Temp Monitor Task",
+        3072,
+        NULL,
+        1,
+        NULL
+    );
 }
